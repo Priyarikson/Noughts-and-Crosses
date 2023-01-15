a3fdc2ffc6191c2085eb9fac887083f1ba0b3b1b
priyanka <priyankaraju92@gmail.com>
2023-01-14 19:20:25 -0500

    working now

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var currentPlayer = "O";
-    var won = false;
+let currentPlayer = "O";
+    let won = false;
     function place(box) {
       if(box.innerText != "" || won) return;
       box.innerText = currentPlayer;
@@ -7,21 +7,26 @@ var currentPlayer = "O";
       checkGameBoard();   
     }
     function checkGameBoard() {
-      for(var i = 0; i <= 2; i++) {
+      for(let i = 0; i <= 2; i++) {
+        //vertical checking
         checkWinner(document.getElementById(i + "_0").innerText,
           document.getElementById(i + "_1").innerText,
           document.getElementById(i + "_2").innerText);
+          //horizontal checking
         checkWinner(document.getElementById("0_" + i).innerText,
           document.getElementById("1_" + i).innerText,
           document.getElementById("2_" + i).innerText);
       }
+      //diagonal checking
       checkWinner(document.getElementById("0_0").innerText,
         document.getElementById("1_1").innerText,
         document.getElementById("2_2").innerText);
+        //diagonal checking
       checkWinner(document.getElementById("0_2").innerText,
         document.getElementById("1_1").innerText,
         document.getElementById("2_0").innerText);
     }
+    //checkwinner function 3 arguments
     function checkWinner(first, second, third) {
       if(first != "" && first == second && first == third) {
         alert("Winner!");
