ceda3856cd02522a20db35cd9a557e55539aec95
priyanka <priyankaraju92@gmail.com>
2023-01-14 19:13:23 -0500

    working now

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-let currentPlayer = "O";
-function place(box) {
-    if(box.innerText !== "") return;
-    box.innerText = currentPlayer;
-    currentPlayer === "O" ? currentPlayer = "X" : currentPlayer = "O";
-    checkGameBoard()
-}
-
-function checkGameBoard() {
-    for(let i=0; i<=2; i++) {
-        let first = document.getElementById("0_" + i).innerText;
-        let second = document.getElementById("1_" + i).innerText;
-        let third = document.getElementById("2_" + i).innerText;
-        if(first === "") continue;
-        if(first === second && first === third) {
-            alert("winner");
-        }
+var currentPlayer = "O";
+    var won = false;
+    function place(box) {
+      if(box.innerText != "" || won) return;
+      box.innerText = currentPlayer;
+      currentPlayer == "O" ? currentPlayer = "X" : currentPlayer = "O";
+      checkGameBoard();   
     }
-    for(let j=0; j<=2; j++) {
-        let first1 = document.getElementById( j + "_0" ).innerText;
-        let second2 = document.getElementById( j + "_1" ).innerText;
-        let third3 = document.getElementById( j + "_2" ).innerText;
-        if(first1 === "") continue;
-        if(first1 === second2 && first1 === third3) {
-            alert("winner");
-        }
+    function checkGameBoard() {
+      for(var i = 0; i <= 2; i++) {
+        checkWinner(document.getElementById(i + "_0").innerText,
+          document.getElementById(i + "_1").innerText,
+          document.getElementById(i + "_2").innerText);
+        checkWinner(document.getElementById("0_" + i).innerText,
+          document.getElementById("1_" + i).innerText,
+          document.getElementById("2_" + i).innerText);
+      }
+      checkWinner(document.getElementById("0_0").innerText,
+        document.getElementById("1_1").innerText,
+        document.getElementById("2_2").innerText);
+      checkWinner(document.getElementById("0_2").innerText,
+        document.getElementById("1_1").innerText,
+        document.getElementById("2_0").innerText);
     }
-}
+    function checkWinner(first, second, third) {
+      if(first != "" && first == second && first == third) {
+        alert("Winner!");
+        won = true;
+      }
+    }
